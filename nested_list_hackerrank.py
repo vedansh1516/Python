f05e57bd8f3b67a3648a5d1d94a222a8eb56b010
Vedansh singhal <vedanshsinghal@gmail.com>
2020-06-22 16:53:17 +0530

    nested_list_hackerrank

---
+++ nested_list_hackerrank.py
@@ -0,0 +1,12 @@
+students= {}
+for i in range(int(input())):
+    name = input()
+    m = float(input())
+    p = float(input())
+    c = float(input())
+    
+    students[name] = m
+    students[name] = p
+    students[name] = c
+
+print(students)    
