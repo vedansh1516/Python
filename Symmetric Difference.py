f50bc553c2b91bb2ee783cc093f21de28874abcc
Vedansh singhal <vedanshsinghal@gmail.com>
2020-07-09 23:51:06 +0530

    hackerrank solution for Symmetric Difference

---
+++ Symmetric Difference.py
@@ -0,0 +1,7 @@
+numbers1 = int(input())
+set1 = set(map(int,input().split()))
+numbers2 = int(input())
+set2 = set(map(int,input().split()))
+set3 = (set1.difference(set2)).union(set2.difference(set1))
+for i in sorted(list(set3)):
+        print(i)
