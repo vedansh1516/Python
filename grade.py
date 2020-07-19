1a6093f33d3b4aaa5c5cdf34e3720fc524483dbd
vedansh <vedanshsinghal@gmail.com>
2020-07-19 23:10:26 +0530

    code to find grades

---
+++ grade.py
@@ -0,0 +1,13 @@
+score = float(input("Enter Score: "))
+if score >= 0.9 :
+    print("A")
+elif score >= 0.8 and score <= 0.9 :
+    print("B")
+elif score >= 0.7 and score <= 0.8:
+    print("C")
+elif score >= 0.6 and score <= 0.7:
+    print("D")
+elif score >= 0.6 and score <= 0.7:
+    print("F")
+else:
+    exit()
