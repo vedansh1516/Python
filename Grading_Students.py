0bb8186194096e0ee0145321d6d8407d80127f92
Vedansh singhal <vedanshsinghal@gmail.com>
2020-08-21 23:09:57 +0530

    solution for Grading Students in hackerrank

---
+++ Grading_Students.py
@@ -0,0 +1,10 @@
+def gradingStudents(grades):
+    for i in range(len(grades)):
+        if (grades[i]<38):
+            print(grades[i])
+        elif(grades[i]%5>=3):
+for _ in range(grades_count):
+    grades_item = int(input().strip())
+    grades.append(grades_item)
+
+result = gradingStudents(grades)
